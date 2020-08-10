7d3bab08842f431fe9cc8b4c46a378d9557b3039
tamdm-dsoft <tamdm@dsoft.com.vn>
2020-08-10 17:28:17 +0700

    adding scratch as the standalone app to experiment the approach before integrating into the system

---
+++ scratch/README.md
@@ -0,0 +1,5 @@
+This folder contains some the test application before we integrated with our system.
+
+eratosthenes: eratosthenes sieve prime test. not reliable and low performance.
+
+
--- scratch/primesieve/main.go
+++ scratch/primesieve/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
 func main() {
-	$END$
+	upperBound := "1234"
+	//./primesieve 1 18446744073709551611 -n
+	cmd := exec.Command("primesieve", "1",  upperBound, "-n")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	output := out.String()
+	if strings.Contains(output, "Nth prime:") {
+		fmt.Print(output[strings.LastIndex(output, "Nth prime: ") + len("Nth prime: "):])
+	}
 }
